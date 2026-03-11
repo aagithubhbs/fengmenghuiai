8e96ac983c988adc3eaf3e68faa6787603171ba0
aagithubhbs <31402631+aagithubhbs@users.noreply.github.com>
2026-03-11 14:05:16 +0800

    Update method01.js

--- method01.js
+++ method01.js
@@ -6,9 +6,6 @@ var prov = document.getElementById('prov');
 var city = document.getElementById('city');
 var country = document.getElementById('country');
 
-if(btn){
-    btn.disabled = true;
-} 
 
 /*用于保存当前所选的省市区*/
 var current = {
